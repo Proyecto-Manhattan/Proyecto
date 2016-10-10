49b4511eeaa3453cbf96623d8664bc41f494a6b0
EMILIANO SOTOMAYOR GONZALEZ <esotomay@itam.mx>
2016-10-10 18:36:55 -0500

    ADDED VISUAL STUDIO

---
+++ Web/html/Default.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Web_html_Default : System.Web.UI.Page
+{
+    private String user;
+    private String pswd;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        user = Request.Form.Get("username");
+        pswd = Request.Form.Get("password");
+
+        Response.Write("<p>" + user + "</p>");
+        Response.Write("<p>" + pswd + "</p>");
+
+
+
+
+        Response.Redirect("index.html?res=none");
+    }
+}
