df268132ac6d310dac7e1c42583aa865f8e6a488
EMILIANO SOTOMAYOR GONZALEZ <esotomayg@itam>
2016-10-27 16:18:00 -0500

    Finished css user cards

--- Testing/TestingAlumno.aspx.cs
+++ Testing/TestingAlumno.aspx.cs
@@ -10,7 +10,7 @@ public partial class Testing_TestingAlumno : System.Web.UI.Page
 {
     protected OdbcConnection conectarBD()
     {
-        String con = "Driver={SQL Server Native Client 11.0};Server=CC201-22;Uid=sa;Pwd=sqladmin;Database=pruebaAlumno;";
+        String con = "Driver={SQL Server Native Client 11.0};Server=112SALAS24;Uid=sa;Pwd=sqladmin;Database=pruebaAlumno;";
         try
         {
             OdbcConnection conexion = new OdbcConnection(con);
@@ -53,14 +53,14 @@ public partial class Testing_TestingAlumno : System.Web.UI.Page
           + "<div class='w3-center w3-text-white'>25%</div></div></div><div class='w3-progress-container'><div id='Div1' class='w3-progressbar w3-red w3-round-xlarge' style='width:25%'>"
           + "<div class='w3-center w3-text-white'>25%</div></div></div><div class='w3-progress-container'><div id='Div2' class='w3-progressbar w3-blue w3-round-xlarge' style='width:25%'>"
           + "<div class='w3-center w3-text-white'>25%</div></div></div><div class='w3-progress-container'><div id='Div3' class='w3-progressbar w3-green w3-round-xlarge' style='width:25%'>"
-          + "<div class='w3-center w3-text-white'>25%</div></div></div><div class='w3-section'><button class='w3-btn w3-green'>Accept</button>"
-          + "<button class='w3-btn w3-red'>Decline</button></div></div></div></section>";
+          + "<div class='w3-center w3-text-white'>25%</div></div></div><div class='w3-section'><button class='w3-btn w3-green'>Ver</button>"
+          + "<button class='w3-btn w3-red'>Baja</button></div></div></div></section>";
 
 
 
 
         String card2 = ""
-        +"<section class='w3-col w3-container m3 l3 w3-itam'>"
+        +"<section class='w3-col w3-container m3 l3 w3-itam  w3-border w3-round-xlarge w3-card-24'>"
         +   "<div class='w3-card-8 w3-dark-grey' style='width:100%'>"
         +       "<div class='w3-container w3-center'>"
         +           "<h3 class='w3-text-lime'>"
@@ -73,16 +73,16 @@ public partial class Testing_TestingAlumno : System.Web.UI.Page
         +           "<h5 id='apellido' runat='server' class='w3-text-white'>"
                         + apellido 
         +           "</h5>"
-        +           "<div id='semestre' class='w3-text-green'>"
-                        +   "Semestre :" + semestre 
-        +           "</div>"
+        +           "<h4 id='semestre' class='w3-text-green'>"
+                     + semestre 
+        +           "</h4>"
         +           "<div id='carrera' runat='server' class='w3-text-amber'>"
                         + carrera 
         +           "</div>"
         +           "<div class='w3-section' style='overflow:hidden;'>"
-        +               "<button class='w3-btn-block w3-green'>Accept</button>"
-        +               "<button class='w3-btn-block w3-red '>Decline</button>"
-        +               "<button class='w3-btn-block w3-blue '>2° Entrevista</button>"
+        +               "<button class='w3-btn-block w3-light-green w3-hover-green'>Ver</button>"
+        +               "<button class='w3-btn-block w3-hover-red'>Elimina</button>"
+        +               "<button class='w3-btn-block w3-light-blue w3-hover-blue'>2° Entrevista</button>"
         +           "</div>"
         +       "</div>"
         +   "</div>"
